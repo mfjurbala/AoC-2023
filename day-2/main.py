0a27e6af1c4c463b199b2bb602d9645a9a6b5168
mfjurbala <mfj.dev@posteo.net>
2023-12-02 23:58:32 -0500

    first pass at day 2 part 1

---
+++ day-2/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+#This is semi-intentionally convoluted. It seemed funny at the time,
+#might come back and try to make it better.
+
+#input = open("input", "r")
+game_count = 1
+total = 0
+
+for line in open("input", "r"):
+    skip = False
+
+    for game in line.rstrip("\n").split(": ")[1].split("; "):
+        dictionary = dict(pull.split(" ") for pull in game.split(", "))
+        correct_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+
+        if "red" in correct_dictionary and int(correct_dictionary["red"]) > 12:
+            skip = True
+        elif "green" in correct_dictionary and int(correct_dictionary["green"]) > 13:
+            skip = True
+        elif "blue" in correct_dictionary and int(correct_dictionary["blue"]) > 14:
+            skip = True
+
+    if not skip:
+        total += game_count
+        print(game_count)
+
+    game_count += 1
+
+print(total)
+
+#for line in input:
+#    games = line.split(":")[1].split(";")
+#
+#    for game in games:
+#        for pull in game.split(","):
+#            print(pull)
+#
+#    game_count += 1
